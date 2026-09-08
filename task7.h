f61a55550d10329f5a5e1929364bac3da743cc1f
ArqamWaheed <arqam.waheed.dev@gmail.com>
2026-09-08 21:54:10 +0500

    Task 7: strassen multiplication, compared against naive one

--- task7.h
+++ task7.h
@@ -6,6 +6,7 @@ const int MAX = 8;
 void addMatrix(int A[][MAX], int B[][MAX], int C[][MAX], int n);
 void subMatrix(int A[][MAX], int B[][MAX], int C[][MAX], int n);
 void naiveMultiply(int A[][MAX], int B[][MAX], int C[][MAX], int n);
+void strassenMultiply(int A[][MAX], int B[][MAX], int C[][MAX], int n);
 void printMatrix(int A[][MAX], int n);
 bool sameMatrix(int A[][MAX], int B[][MAX], int n);
 
